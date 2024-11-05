85b63b2c3c4fd2cbead32ea0f769742fcca16131
Victory Chibuike <victorychibuike121@gmail.com>
2024-11-05 23:19:10 +0100

    Made file executable

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+"""Basic Flask Application"""
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Index page"""
     return render_template('0-index.html')
